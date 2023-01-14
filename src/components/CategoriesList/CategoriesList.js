a1e1d365ee1806027d08e9d7100d2d05af8ea8ec
harshitchopra7 <chopraharshit2000@gmail.com>
2023-01-14 12:46:06 +0530

    image links

---
+++ src/components/CategoriesList/CategoriesList.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+// images link
+// 1. Top offers - https://rukminim1.flixcart.com/fk-p-flap/128/128/image/47e5c31ef7dcce1e.png?q=100
+// 2. Mobiles - https://rukminim1.flixcart.com/fk-p-flap/128/128/image/5f8bd5669e19e724.png?q=100
+// 3. Electronics - https://rukminim1.flixcart.com/fk-p-flap/128/128/image/361d53b8725c2d2d.png?q=100
+// 4.  TVs - https://rukminim1.flixcart.com/fk-p-flap/128/128/image/904f3c8e7101408b.png?q=100
+// 5. Fashion - https://rukminim1.flixcart.com/fk-p-flap/128/128/image/7f7355480c6adc16.png?q=100
+// 6. Beauty - https://rukminim1.flixcart.com/fk-p-flap/128/128/image/f97d6138516056bc.png?q=100
+// 7. Furniture - https://rukminim1.flixcart.com/fk-p-flap/128/128/image/6728ed3cf145562c.png?q=100
+// 8. https://rukminim1.flixcart.com/fk-p-flap/128/128/image/007910082ae6355b.png?q=100
+
+function CategoriesList() {
+  return (
+    <div>
+      <p>I am a category list</p>
+    </div>
+  );
+}
+export default CategoriesList;
