1fc45f351c2c26caa3f58be315925e9e45dc3bf8
harshitchopra7 <chopraharshit2000@gmail.com>
2023-01-14 16:33:22 +0530

    reusable components

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
   return (
     <div className="app">
       <Navbar />
+
       <CategoriesList />
       {/* <Carousel /> */}
     </div>
---
+++ src/components/CategoriesList/Card.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Card.css";
+
+// general component
+// structure
+
+// Destructuring - { image, text  }
+
+function Card({ image, text }) {
+  return (
+    <div className="single_category">
+      <img src={image} />
+      <p>{text}</p>
+    </div>
+  );
+}
+export default Card;
